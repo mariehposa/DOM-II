e911eb67bd054360407b9b151d4bd8c577aabe93
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-03 16:09:49 +0100

    nest nav and class navlink

--- js/index.js
+++ js/index.js
@@ -2,11 +2,20 @@
 const navs = document.querySelectorAll('.nav-link')
 
 navs.forEach(nav => {
-    nav.addEventListener('click', (event) => {event.preventDefault(); console.log(event)})
+    nav.addEventListener('click', (event) => {
+        event.preventDefault(); 
+        console.log(event)
+    })
 
     nav.addEventListener('mouseenter', (e) => e.target.style.color = 'silver')
     nav.addEventListener('mouseleave', (e) => e.target.style.color = 'black')
 
+})
+
+document.querySelector('.nav').addEventListener('click', (e) => {
+    e.target.style.backgroundColor = 'dodgerblue'
+    e.target.style.padding = '3rem 0'
+    e.target.style.color = 'white'
 })
 
 const images = document.querySelectorAll('img')
@@ -25,4 +34,29 @@ images.forEach(image => {
         event.target.style.transition = '0.3s'
         event.target.style.transform = 'scale(0.5)'
     })
+})
+
+let buttons = document.querySelectorAll('.btn')
+
+buttons.forEach(button => {
+    button.addEventListener('mousedown', (event) => {
+        event.target.style.backgroundColor = 'orange'
+        event.target.style.color = 'white'
+    })
+
+    button.addEventListener('mouseup', (event) => {
+        event.target.style.backgroundColor = '#17A2B8'
+        event.target.style.color = 'white'
+    })
+
+    button.addEventListener('mouseout', (e) => {
+        e.target.style.backgroundColor = '#17A2B8'
+        event.target.style.color = 'white'
+    })
+
+    button.addEventListener('mouseover', (e) => {
+        e.target.style.color = '#17A2B8'
+        event.target.style.backgroundColor = 'white'
+    })
+        
 })
