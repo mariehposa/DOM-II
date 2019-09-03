593907fe479e98d8c7058e8866daccac6e32f579
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-09-03 14:59:33 +0100

    Add 3 events to nav items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const navs = document.querySelectorAll('.nav-link')
+
+navs.forEach(nav => {
+    nav.addEventListener('click', (event) => {event.preventDefault(); console.log(event)})
+
+    nav.addEventListener('mouseenter', (e) => e.target.style.color = 'silver')
+    nav.addEventListener('mouseleave', (e) => e.target.style.color = 'black')
+
+})
